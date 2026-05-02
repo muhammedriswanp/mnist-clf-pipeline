49faeb598580667a1688674b2fbd79bb6fdc4f7b
Muhammed Riswan P <muhammedriswanp7@gmail.com>
2026-05-02 19:33:49 +0530

    add model architecture - MNISTClassifier

---
+++ training/model.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+
+class MNISTClassifier(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+        self.network = nn.Sequential(
+            nn.Linear(784, 256),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+
+            nn.Linear(256,120),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+
+            nn.Linear(120,10)
+        )
+
+    def forward(self, x):
+        x = x.view(-1, 784)
+        return self.network(x)
